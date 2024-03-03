395b4fa84a7148507141af0a139d2fd137cdd501
najro <orjan@najro.com>
2024-03-03 17:40:16 +0100

    lab3 - added code for dragn drop and updated comments

--- lab3/TextEditor/MainPage.xaml.cs
+++ lab3/TextEditor/MainPage.xaml.cs
@@ -1,24 +1,28 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Windows.ApplicationModel.Core;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.Storage;
 using Windows.Storage.Pickers;
-using Windows.UI.Core.Preview;
-using Windows.UI.Popups;
+using Windows.System;
+using Windows.UI.Core;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// This is a modified version of the Blank Page item template to create a simple text editor
 
 namespace TextEditor
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// This is the main page of the text editor app
+    /// Backend code :  (lab3 - alfoande100 / Örjan Andersson)
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        private bool _isTextChanged = false;
+        private bool _isTextChanged = false; // Hold the state of the text input
         private bool _openNewOrStartOver = true;
         private const string TextExtension = ".txt";
         private const string DefaultFileName = "dok1.txt";
@@ -35,8 +39,7 @@ namespace TextEditor
         private const string UnsavedChangesSecondaryButtonTextNo = "Nej";
         private const string UnsavedChangesCloseButtonTextCancel = "Avbryt";
 
-
-        private StorageFile CurrentStorageFile = null;
+        private StorageFile CurrentStorageFile = null; // Hold the current file in memory
 
         public MainPage()
         {
@@ -45,6 +48,11 @@ namespace TextEditor
             SetAppTitle(CurrentFileName);
         }
 
+        /// <summary>
+        /// Update the app title and metadata when the text is changed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void TextInputBox_OnTextChanged(object sender, TextChangedEventArgs e)
         {
             if (_openNewOrStartOver)
@@ -63,14 +71,13 @@ namespace TextEditor
 
             SetAppTitle(CurrentFileName);
             UpdateMetaDataInfo();
-
         }
 
+        /// <summary>
+        /// Update the metadata info
+        /// </summary>
         private void UpdateMetaDataInfo()
         {
-
-            // TODO add reference to split documentation from microsoft
-
             var numberOfCharactersIncludingSpace = TextInputBox.Text.Length;
             var numberOfCharactersWithoutSpace = TextInputBox.Text.Replace(" ", "").Length;
             var numberOfWords = TextInputBox.Text.Split(new[] { ' ', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries).Length;
@@ -80,10 +87,12 @@ namespace TextEditor
             TextBlockNumberCharactersWithoutSpace.Text = string.Format(MetaDataNumberCharactersWithoutSpace, numberOfCharactersWithoutSpace);
             TextBlockNumberWords.Text = string.Format(MetaDataNumberWords, numberOfWords);
             TextBlockNumberLines.Text = string.Format(MetaDataNumberLines, numberOfLines);
-
-
         }
 
+        /// <summary>
+        /// Set the app title
+        /// </summary>
+        /// <param name="title"></param>
         private void SetAppTitle(string title)
         {
             if (_isTextChanged)
@@ -95,7 +104,9 @@ namespace TextEditor
             ApplicationView.GetForCurrentView().Title = title;
         }
 
-
+        /// <summary>
+        /// Clear the text input and reset the state to clean start
+        /// </summary>
         private void ClearTextInput()
         {
             TextInputBox.Text = string.Empty;
@@ -108,14 +119,16 @@ namespace TextEditor
             UpdateMetaDataInfo();
         }
 
-
+        /// <summary>
+        ///  Handle the open file button click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void AppBarButtonOpen_OnClick(object sender, RoutedEventArgs e)
         {
-
-
             if (_isTextChanged)
             {
-                // add documentation for ContentDialog
+                // https://docs.microsoft.com/en-us/windows/uwp/design/controls-and-patterns/dialogs-and-flyouts/dialogs
                 var dialog = new ContentDialog
                 {
                     Title = UnsavedChangesTitle,
@@ -127,13 +140,15 @@ namespace TextEditor
 
                 dialog.PrimaryButtonClick += async (s, args) =>
                 {
+                    // if the user clicks yes, save the file and then open a new file
                     await SaveFile();
                     OpenFile();
                 };
 
                 dialog.SecondaryButtonClick += (s, args) =>
                 {
-                     OpenFile();
+                    // if the user clicks no, open a new file
+                    OpenFile();
                 };
 
                 await dialog.ShowAsync();
@@ -145,6 +160,108 @@ namespace TextEditor
 
         }
 
+        /// <summary>
+        /// Handle the save button click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void AppBarButtonSave_OnClick(object sender, RoutedEventArgs e)
+        {
+            await SaveFile();
+        }
+        /// <summary>
+        /// Handle the save as button click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void AppBarButtonSaveAs_OnClick(object sender, RoutedEventArgs e)
+        {
+            SaveFile(forceFilePicker: true);
+        }
+
+        /// <summary>
+        /// Handle the new button click. If the text is changed, ask the user if they want to save the file before creating a new file.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void AppBarButtonNew_OnClick(object sender, RoutedEventArgs e)
+        {
+
+            if (_isTextChanged)
+            {
+                var dialog = new ContentDialog
+                {
+                    Title = UnsavedChangesTitle,
+                    Content = UnsavedChangesContent,
+                    PrimaryButtonText = UnsavedChangesPrimaryButtonTextYes,
+                    SecondaryButtonText = UnsavedChangesSecondaryButtonTextNo,
+                    CloseButtonText = UnsavedChangesCloseButtonTextCancel
+                };
+
+                dialog.PrimaryButtonClick += async (s, args) =>
+                {
+                    // if the user clicks yes, save the file and then start over with a new text input and file
+                    await SaveFile();
+                    ClearTextInput();
+                };
+
+                dialog.SecondaryButtonClick += (s, args) =>
+                {
+                    // if the user clicks no, startover with a new text input and file
+                    ClearTextInput();
+                };
+
+                await dialog.ShowAsync();
+            }
+            else
+            {
+                ClearTextInput();
+            }
+        }
+
+        /// <summary>
+        /// Handle the close button click. If the text is changed, ask the user if they want to save the file before closing the app.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void AppBarButtonClose_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (_isTextChanged)
+            {
+                var dialog = new ContentDialog
+                {
+                    Title = UnsavedChangesTitle,
+                    Content = UnsavedChangesContent,
+                    PrimaryButtonText = UnsavedChangesPrimaryButtonTextYes,
+                    SecondaryButtonText = UnsavedChangesSecondaryButtonTextNo,
+                    CloseButtonText = UnsavedChangesCloseButtonTextCancel
+                };
+
+                dialog.PrimaryButtonClick += async (s, args) =>
+                {
+                    // if the user clicks yes, save the file and then close the app
+                    await SaveFile();
+                    CoreApplication.Exit();
+                };
+
+                dialog.SecondaryButtonClick += (s, args) =>
+                {
+                    // if the user clicks no, close the app
+                    CoreApplication.Exit();
+                };
+
+                await dialog.ShowAsync();
+            }
+            else
+            {
+                // https://learn.microsoft.com/en-us/uwp/api/windows.applicationmodel.core.coreapplication.exit?view=winrt-22621
+                CoreApplication.Exit();
+            }
+        }
+
+        /// <summary>
+        /// Open a file and set the text input to the content of the file. Hold the file in memory for later use. Update the metadata info.
+        /// </summary>
         private async void OpenFile()
         {
             var fileOpenPicker = new FileOpenPicker();
@@ -163,11 +280,11 @@ namespace TextEditor
             UpdateMetaDataInfo();
         }
 
-        private async void AppBarButtonSave_OnClick(object sender, RoutedEventArgs e)
-        {
-            await SaveFile();
-        }
-
+        /// <summary>
+        /// Save the file. If the file is already saved, save it. If not, open a file picker and save it.
+        /// </summary>
+        /// <param name="forceFilePicker"></param>
+        /// <returns></returns>
         private async Task SaveFile(bool forceFilePicker = false)
         {
             if (CurrentStorageFile != null && forceFilePicker != true)
@@ -194,98 +311,63 @@ namespace TextEditor
             }
         }
 
-        private async void AppBarButtonSaveAs_OnClick(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Handle the drop event when the user drops a file into the text input
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void TextInputBox_OnDrop(object sender, DragEventArgs e)
         {
-            SaveFile(forceFilePicker:true);
-            //var fileSavePicker = new FileSavePicker();
-            //fileSavePicker.FileTypeChoices.Add("Plain Text", new[] { TextExtension });
+            // https://mzikmund.dev/blog/the-right-way-to-check-for-key-state-in-uwp-apps
+            // https://learn.microsoft.com/en-us/uwp/api/windows.ui.core.corevirtualkeystates?view=winrt-22621
+            var ctrlPressed =  Window.Current.CoreWindow.GetKeyState(VirtualKey.Control).HasFlag(CoreVirtualKeyStates.Down);
+            var shiftPressed = Window.Current.CoreWindow.GetKeyState(VirtualKey.Shift).HasFlag(CoreVirtualKeyStates.Down);
+            var textFromFile = string.Empty;
 
-            //var result = await fileSavePicker.PickSaveFileAsync();
-
-            //if (result != null)
-            //{
-            //    await FileIO.WriteTextAsync(result, TextInputBox.Text);
-            //    CurrentStorageFile = result;
-            //    CurrentFileName = result.Name;
-            //    _isTextChanged = false;
-            //    SetAppTitle(CurrentFileName);
-            //}
-        }
-
-        private async void AppBarButtonNew_OnClick(object sender, RoutedEventArgs e)
-        {
-
-            if (_isTextChanged)
+            // https://learn.microsoft.com/en-us/windows/apps/design/input/drag-and-drop
+            if (e.DataView.Contains(StandardDataFormats.StorageItems))
             {
-                // add documentation for ContentDialog
-                var dialog = new ContentDialog
-                {
-                    Title = UnsavedChangesTitle,
-                    Content = UnsavedChangesContent,
-                    PrimaryButtonText = UnsavedChangesPrimaryButtonTextYes,
-                    SecondaryButtonText = UnsavedChangesSecondaryButtonTextNo,
-                    CloseButtonText = UnsavedChangesCloseButtonTextCancel
-                };
+                var items = await e.DataView.GetStorageItemsAsync();
 
-                dialog.PrimaryButtonClick += async (s, args) =>
+                if (items.Count > 0)
                 {
-                    //AppBarButtonSave_OnClick(sender, e);
-                    await SaveFile();
-                    ClearTextInput();
-                };
+                    var storageFile = items.First() as StorageFile;
 
-                dialog.SecondaryButtonClick += (s, args) =>
-                {
-                    ClearTextInput();
-                };
+                    if (storageFile != null && storageFile.FileType == TextExtension)
+                    {
+                        textFromFile = await FileIO.ReadTextAsync(storageFile);
 
-                await dialog.ShowAsync();
-            }
-            else
-            {
-                ClearTextInput();
+                        if (ctrlPressed)
+                        {
+                            TextInputBox.Text += textFromFile;
+                        }
+                        else if (shiftPressed)
+                        {
+                            var cursorPosition = TextInputBox.SelectionStart;
+                            TextInputBox.Text = TextInputBox.Text.Insert(cursorPosition, textFromFile);
+                        }
+                        else
+                        {
+                            ClearTextInput();
+                            TextInputBox.Text = textFromFile;
+                            UpdateMetaDataInfo();
+                            SetAppTitle(CurrentFileName);
+                        }
+                    }
+                }
             }
         }
 
-        private async void AppBarButtonClose_OnClick(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Change the drag effect to copy when the user drags a file over the text input
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TextInputBox_OnDragOver(object sender, DragEventArgs e)
         {
-
-            if (_isTextChanged)
-            {
-                // add documentation for ContentDialog
-                var dialog = new ContentDialog
-                {
-                    Title = UnsavedChangesTitle,
-                    Content = UnsavedChangesContent,
-                    PrimaryButtonText = UnsavedChangesPrimaryButtonTextYes,
-                    SecondaryButtonText = UnsavedChangesSecondaryButtonTextNo,
-                    CloseButtonText = UnsavedChangesCloseButtonTextCancel
-                };
-
-                dialog.PrimaryButtonClick += async (s, args) =>
-                {
-                    await SaveFile();
-                    CoreApplication.Exit();
-                };
-
-                dialog.SecondaryButtonClick += (s, args) =>
-                {
-                    CoreApplication.Exit();
-                };
-
-                await dialog.ShowAsync();
-            }
-            else
-            {
-                // https://learn.microsoft.com/en-us/uwp/api/windows.ui.xaml.application.exit?view=winrt-22621
-                CoreApplication.Exit();
-            }
-
-        }
-
-        private void TextInputBox_OnDrop(object sender, DragEventArgs e)
-        {
-            throw new NotImplementedException();
+            // https://learn.microsoft.com/en-us/windows/apps/design/input/drag-and-drop
+            // https://www.c-sharpcorner.com/UploadFile/6d1860/implementing-file-drag-drop-in-your-windows-10-uwp-applica/
+            e.AcceptedOperation = DataPackageOperation.Copy;
         }
     }
 }
